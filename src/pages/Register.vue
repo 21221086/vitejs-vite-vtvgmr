d7bf23569ed99b322f671049a63a19d6a3697db7
21221086 <54755443+21221086@users.noreply.github.com>
2022-09-14 15:49:03 -0400

    4 files modified, 2 added

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -8,68 +8,98 @@
   
     <div class="flex justify-center">
       <div class="flex">
-        <h1 class="text-gray-600 font-bold md:text-2xl text-xl">
-          League Registration
           <div v-if="league">For {{ JSON.parse(league).sport }} </div>
+      </div>
+    </div>
+
+    <div class="grid">
+      <!-- SPORT -->
+      <div class="pt-5 pb-5" v-if="formToShow === 'Sport'">
+        <h1 class="text-gray-600 font-bold md:text-2xl text-xl">
+          League Information
         </h1>
-      </div>
-    </div>
-
-    <div class="grid grid-cols-2">
-      <div class="grid grid-cols-1 mt-5 mx-7">
-        <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Sport</label>
-        <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
-          <option>Option 1</option>
-          <option>Option 2</option>
-          <option>Option 3</option>
-        </select>
-      </div>
-
-      <div class="grid grid-cols-1 mt-5 mx-7">
-        <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">League</label>
-        <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
-          <option>Option 1</option>
-          <option>Option 2</option>
-          <option>Option 3</option>
-        </select>
-      </div>
-
-      <div class="grid grid-cols-1 mt-5 mx-7">
-        <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">League</label>
-        <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
-          <option>Option 1</option>
-          <option>Option 2</option>
-          <option>Option 3</option>
-        </select>
-      </div>
-
-      <div class="grid grid-cols-1 mt-5 mx-7">
-        <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 1</label>
-        <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Input 1" />
-      </div>
-
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
-        <div class="grid grid-cols-1">
-          <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 2</label>
-          <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Input 2" />
+        <div class="grid grid-cols-1 mt-5 mx-7">
+          <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Sport</label>
+          <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+            <option>Baseball</option>
+            <option>Soccer</option>
+            <option>Football</option>
+          </select>
         </div>
-        <div class="grid grid-cols-1">
-          <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 3</label>
-          <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Input 3" />
+
+        <div class="grid grid-cols-1 mt-5 mx-7">
+          <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Season</label>
+          <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+            <option>Fall 2020</option>
+            <option>Winter 2020</option>
+            <option>Option 3</option>
+          </select>
+        </div>
+
+        <div class="grid grid-cols-1 mt-5 mx-7">
+          <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">League</label>
+          <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+            <option>Recreational</option>
+            <option>Competetive</option>
+            <option>Elite</option>
+          </select>
+        </div>
+
+        <div class="grid grid-cols-1 mt-5 mx-7">
+          <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Registration Type</label>
+          <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+            <option>Team</option>
+            <option>Player</option>
+          </select>
+        </div>
+
+        <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
+          <router-link to="/"><button class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Cancel</button></router-link>
+          <button v-on:click="displayForm('Player')" class='w-auto bg-purple-500 hover:bg-purple-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Next</button>
         </div>
       </div>
 
-      <div class="grid grid-cols-1 mt-5 mx-7">
-        <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Another Input</label>
-        <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Another Input" />
+      <!-- PLAYER INFO -->
+      <div class="pt-5 pb-5" v-if="formToShow === 'Player'">
+        <h1 class="text-gray-600 font-bold md:text-2xl text-xl">
+          Player Information
+        </h1>
+        <div class="grid grid-cols-1 mt-5 mx-7">
+          <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 1</label>
+          <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Input 1" />
+        </div>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
+          <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 2</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Input 2" />
+          </div>
+          <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 3</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Input 3" />
+          </div>
+        </div>
+        <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
+          <button v-on:click="displayForm('Sport')" class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Back</button>
+          <button v-on:click="displayForm('Parent')" class='w-auto bg-purple-500 hover:bg-purple-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Next</button>
+        </div>
       </div>
 
-      <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
-        <button class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Cancel</button>
-        <button class='w-auto bg-purple-500 hover:bg-purple-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Create</button>
+      <div class="pt-5 pb-5" v-if="formToShow === 'Parent'">
+        <h1 class="text-gray-600 font-bold md:text-2xl text-xl">
+          Parent Information
+        </h1>
+        <div class="grid grid-cols-1 mt-5 mx-7">
+          <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Another Input</label>
+          <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Another Input" />
+        </div>
+
+        <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
+          <button v-on:click="displayForm('Player')" class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Back</button>
+          <button v-on:click="submitForm" class='w-auto bg-purple-500 hover:bg-purple-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Submit</button>
+        </div>
       </div>
     </div>
-
   </div>
 </div>
 </template>
@@ -83,10 +113,20 @@ import FrontPage from '../pages/FrontPage.vue'
 export default {
   data: function () {
     return {
-      showFrontPage: true
+      showFrontPage: true,
+      formToShow: 'Sport'
     }
   },
   props: ['league'],
   components: { FrontPage, Banner, NavigationBar  },
+  methods: {
+    displayForm (formToShow) {
+      console.log('formToShow: ', formToShow)
+      this.formToShow = formToShow
+    },
+    submitForm () {
+      console.log('Form Submitted')
+    }
+  }
 };
 </script>
