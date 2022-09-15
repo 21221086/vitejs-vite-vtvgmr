06869d8b9cda627df056df5af1656286a61c701d
21221086 <54755443+21221086@users.noreply.github.com>
2022-09-15 17:12:04 -0400

    1 file modified

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,29 @@
 <template>
  <NavigationBar></NavigationBar>
- This is the About page
  <Banner></Banner>
+  <div class="w-full flex flex-col flex-row">
+    <div class="bg-zinc-900 p-40">
+      <h1 class="text-2xl md:text-4xl text-stone-100">Here at OliviaSports we are offering a different approach to sports!</h1>
+      <br>
+      <div class="text-stone-100">
+        Some people love the daily grind and fierce competition of sports.  Others just want the opportunity to learn. Some just want to play competetive sports without dedicating their life to it.  <br><br>We believe that every kid should get to play every sport at their level.
+      </div>
+      <br>
+      <div class="text-stone-100">
+        Our goal is to offer as many sports as possible at 3 distinct levels.  
+          <ol type="A">
+            <li>Elite</li>
+            <li>Competetive</li>
+            <li>Recreation</li>
+          </ol>
+      </div>
+      <br />
+    </div>
+  </div>
+
+  <footer class="bg-slate-800 mt-auto p-5">
+    <h1 class="text-2xl md:text-4xl text-white">Footer</h1>
+  </footer>
  </template>
 
 <script>
