18eac3607988e903f6ee3e8b4f994c3414dfbc6a
21221086 <54755443+21221086@users.noreply.github.com>
2022-09-15 12:47:58 -0400

    1 file modified, 1 added

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -46,7 +46,7 @@
         </div>
 
         <div class="grid grid-cols-1 mt-5 mx-7">
-          <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Registration Type</label>
+          <label class="border text-left uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Registration Type</label>
           <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
             <option>Team</option>
             <option>Player</option>
